fbada28cd5e6d87a524438bc1c1b715a3516184f
johnlai <johntyty912@gmail.com>
2019-05-27 16:12:28 +0800

    Wrap longer lines

--- lib/main.dart
+++ lib/main.dart
@@ -51,8 +51,8 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
               child: new IconButton(
                 icon: new Icon(Icons.send),
                 onPressed: _isComposing
-                ? () => _handleSubmitted(_textController.text)
-                : null,
+                    ? () => _handleSubmitted(_textController.text)
+                    : null,
               ),
             )
           ],
@@ -129,15 +129,17 @@ class ChatMessage extends StatelessWidget {
               margin: const EdgeInsets.only(right: 16.0),
               child: new CircleAvatar(child: new Text(_name[0])),
             ),
-            new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(_name, style: Theme.of(context).textTheme.subhead),
-                new Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: new Text(text),
-                ),
-              ],
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(_name, style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text),
+                  ),
+                ],
+              ),
             )
           ],
         ),
