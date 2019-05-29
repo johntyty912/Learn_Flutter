750afcb7f8a2f517d665beeddcd257a3bac779f2
johnlai <johntyty912@gmail.com>
2019-05-29 18:20:57 +0800

    specify dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -110,14 +111,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushNamed(context, "new_page");
               },
             ),
-            Divider(),
-            CupertinoButton(
-              child: Text('open echo route'),
-              color: Colors.blue,
-              onPressed: () {
-                Navigator.pushNamed(context, "echo_page", arguments: _counter);
-              },
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CupertinoButton(
+                child: Text('open echo route'),
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.pushNamed(context, "echo_page", arguments: _counter);
+                },
+              ),
             ),
+            RandomWordsWidget(),
           ],
         ),
       ),
@@ -158,4 +162,16 @@ class EchoRoute extends StatelessWidget {
       ),
     );
   }
+}
+
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(wordPair.asPascalCase),
+    );
+  }
+  
 }
